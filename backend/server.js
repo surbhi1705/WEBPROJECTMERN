3271f5eb277aeffeb47e0b01190b8a3181e3a007
surbhi1705 <118654793+surbhi1705@users.noreply.github.com>
2024-06-16 09:50:18 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,13 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-const PORT = process.env.PORT || 5000;
+
 
 const __dirname = path.resolve();
 
 dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser()); // to parse the incoming cookies from req. cookies
